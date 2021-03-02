7b0b8f5419c800ced6786a2f882ca2c0164d6f85
domds1998 <dominik.suchner@gmail.com>
2021-03-02 03:10:31 +0100

    Timeline has been styled

--- juicy-notes-webapp/src/pages/TimelinePage.js
+++ juicy-notes-webapp/src/pages/TimelinePage.js
@@ -2,18 +2,24 @@ import React from 'react';
 import { ContentWrapper, CollectionWrapper, CollectionItem, CollectionPin } from '../styles/TimelineStyles';
 
 let array = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
+let colors = ["#967AFA", "#9CD964", "#F6FC79", "#25D946"]
 
 function renderTimeLine() {
 
     let counter = 1;
 
-    let result = array.map(value =>
-        <>
-            <CollectionPin>{value}</CollectionPin>
-            <CollectionItem background="#eaeaea" columnStart={counter % 2 === 0 ? "3" : "1"} rowStart={counter} rowEnd={counter++ + 2}>
+    let result = array.map(value => {
+
+        // var randomColor = Math.floor(Math.random() * (16777215 - 100 + 1)).toString(16);
+        var randomColor = Math.floor(Math.random() * (16777215 - 10000 + 1)).toString(16);
+
+        return <>
+            <CollectionPin direction={counter % 2 === 0 ? "left" : "right"} background={'#' + randomColor}>{value}</CollectionPin>
+            <CollectionItem direction={counter % 2 === 0 ? "left" : "right"} borderColor={'#' + randomColor} background="#eaeaea" columnStart={counter % 2 === 0 ? "3" : "1"} rowStart={counter} rowEnd={counter++ + 2}>
                 {value}
             </CollectionItem>
         </>
+    }
     );
 
     return result;
--- juicy-notes-webapp/src/styles/TimelineStyles.js
+++ juicy-notes-webapp/src/styles/TimelineStyles.js
@@ -27,11 +27,14 @@ export const Title = styled.p`
 
 export const CollectionWrapper = styled.div`
     width: 100%;
-    height: 100%;
+    max-width: 100%;
     grid-auto-flow: row;
     display: grid;
-    grid-template-columns: 47.5% 15% 47.5%;
-    grid-auto-rows: 100px;
+    grid-template-columns:     
+        [full-start] minmax(1em, 47.5%) 
+        [main-start] minmax(0, 15%) [main-end]
+        minmax(1em, 47.5%) [full-end];;
+    grid-auto-rows: 75px;
     grid-gap: 5px;
     padding-bottom: 50px;
     padding-top: 50px;
@@ -42,9 +45,28 @@ export const CollectionItem = styled.div`
     grid-column-start: ${props => props.columnStart};
     grid-row-start: ${props => props.rowStart};
     grid-row-end: ${props => props.rowEnd};
+    margin-right: ${props => props.direction === "right" ? "35px" : "0px"};
+    margin-left: ${props => props.direction === "left" ? "35px" : "0px"};
+    border-left: ${props => props.direction === "left" ? "5px solid " + props.borderColor : "none"};
+    border-right: ${props => props.direction === "right" ? "5px solid " + props.borderColor : "none"};
 `;
 
 export const CollectionPin = styled.div`
-    background-color: blue;
+    background-color: ${props => props.background};
     grid-column-start: 2;
+    position: relative;
+
+    &::before{
+        content: "";
+        position: absolute;
+        left: ${props => props.direction === "right" ? "-30px" : "auto"};
+        right: ${props => props.direction === "left" ? "-30px" : "0px"};
+        bottom: 0;
+        width: 0;
+        height: 0;
+        border-right: ${props => props.direction === "right" ? "30px solid " + props.background : "none"};
+        border-left: ${props => props.direction === "left" ? "30px solid " + props.background : "none"};
+        border-top: 37.5px solid transparent;
+        border-bottom: 37.5px solid transparent;
+    }
 `;
